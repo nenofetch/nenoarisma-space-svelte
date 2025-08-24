35c4b4d564c484a54d8a38fb01761488ec4a1231
Neno Arisma <nenoarisma@gmail.com>
2025-08-24 21:46:37 +0700

    add hero component with personal introduction and contact link

---
+++ src/components/hero.svelte
@@ -0,0 +1,36 @@
+<script lang="ts"></script>
+
+<div class="container">
+  <div class="flex flex-wrap">
+    <div class="w-full self-center px-4 lg:w-1/2">
+      <h1 class="text-base font-semibold text-primary lg:text-2xl">
+        Hi 👋, <span
+          class="block font-bold text-slate-900 text-4xl mt-1 lg:text-5xl"
+          >Neno Arisma</span
+        >
+      </h1>
+      <h2 class="text-slate-500 font-medium text-lg mb-5 lg:text-2xl">
+        Full-Stack Web3 Developer, DevOps Engineer, Infrastructure and AI
+        Engineer
+      </h2>
+      <p class="font-medium text-slate-400 mb-10 leading-relaxed">
+        An ordinary people who have interest with Technology, Space and History.
+      </p>
+      <a
+        href="https://github.com/nenofetch"
+        class="bg-primary py-3 px-8 text-base font-semibold text-white hover:shadow-lg hover:opacity-80 rounded-full transition duration-300 ease-in-out"
+        >Contact Me</a
+      >
+    </div>
+
+    <div class="w-full self-end px-4 lg:w-1/2">
+      <div class="mt-10 relative lg:mt-9 lg:right-0">
+        <img
+          src="https://avatars.githubusercontent.com/u/22952560?v=4"
+          alt="My Profile"
+          class="max-w-full mx-auto rounded-full object-cover"
+        />
+      </div>
+    </div>
+  </div>
+</div>
