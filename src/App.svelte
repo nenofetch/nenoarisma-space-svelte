f1f9485b9c76065529b8e47a3ffbb5a92941bd30
Neno Arisma <nenoarisma@gmail.com>
2025-08-24 21:45:59 +0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Hero from "./components/hero.svelte";
+</script>
+
+<main class="min-h-screen">
+  <!-- Hero Section Start -->
+  <section id="hero" class="pt-36">
+    <Hero />
+  </section>
+  <!-- Hero Section End -->
+</main>
