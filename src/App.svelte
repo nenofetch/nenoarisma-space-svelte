2bdf44d95d72c4e3663e4b01b39d2ea32b4734ed
Neno Arisma <nenoarisma@gmail.com>
2025-08-28 11:12:16 +0700

    feat: add header component with responsive navigation and scroll behavior

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
   import About from "./components/about.svelte";
+  import Header from "./components/header.svelte";
   import Hero from "./components/hero.svelte";
 </script>
 
 <main class="min-h-screen">
+  <!--> Header Section Start -->
+  <Header />
+  <!--> Header Section End</!-->
   <!-- Hero Section Start -->
   <section id="hero" class="pt-36">
     <Hero />
   </section>
   <!-- Hero Section End -->
-   <!-- About Section Start -->
-    <section id="about" class="pt-36 pb-32">
-      <About />
-    </section>
-   <!-- About Section End -->
+  <!-- About Section Start -->
+  <section id="about" class="pt-36 pb-32">
+    <About />
+  </section>
+  <!-- About Section End -->
 </main>
